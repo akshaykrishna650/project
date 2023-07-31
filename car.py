1f0cb1a59f9bacaee679dda461592dd5a89156f1
akshaykrishna650 <115461410+akshaykrishna650@users.noreply.github.com>
2023-07-31 20:47:21 +0530

    Add files via upload

---
+++ car.py
@@ -0,0 +1,3 @@
+from sketchpy import canvas
+obj=canvas.sketch_from_image(r"C:\Users\acer\Desktop\project 3\car.png")
+obj.draw()
---
+++ temp.py
@@ -0,0 +1,3 @@
+from sketchpy import library
+obj=library.rdj()
+obj.draw()
